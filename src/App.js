d538beb4127859ff03c00b764f97678b4e19512f
codewithkevin <oseikelvin709@gmail.com>
2022-06-18 17:19:13 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <>
+      <Navbar />
+      
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+        <h1>NETFLIX</h1>
+        <button>Sign In</button>
+        <button>Sign Up</button>
+    </div>
+  )
+}
+
+export default Navbar;
