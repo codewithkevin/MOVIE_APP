7a3c987d2ffbf1667f29f34ebb599046fc6bf238
codewithkevin <oseikelvin709@gmail.com>
2022-06-18 22:28:20 +0000

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ export const Navbar = () => {
         <h1 className='text-red-600 text-4xl text
         font-bold cursor-pointer'>NETFLIX</h1>
         <button className=''>Sign In</button>
-        <button className='bg-red-600 px-6 py-2'>Sign Up</button>
+        <button className='bg-red-600 px-6 py-4
+        rounded cursor-pointer text-white'>Sign Up</button>
     </div>
   )
 }
