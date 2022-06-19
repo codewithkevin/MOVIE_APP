8a4c329124f6229652b0dc96847176f2e4510f6c
codewithkevin <oseikelvin709@gmail.com>
2022-06-19 13:37:04 +0000

    update

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -16,7 +16,7 @@ export const Row = ({title, fetchURL}) => {
     const cards = movies.map((movie) => {
         return(
             <div className='w-[160px> sm:w-[240px] lg:w-[180px] inline-block cursor-pointer relative p-2'>
-                    <img src='' alt={movie?.title}/>
+                    <img src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`} alt={movie?.title}/>
                     
             </div>
         )
