f6010adbb2a7d4bae27d8501c1427fcac38b0200
codewithkevin <oseikelvin709@gmail.com>
2022-06-19 09:41:22 +0000

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -23,8 +23,8 @@ export const Main = () => {
             <img className='w-full h-full object-cover' 
             src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} 
             alt={movie?.title} />
-            <div className='aboslute w-full top- p-4 md:p-8'>
-                    <h1 className="text-3xl md:text-5xl">{movie?.title}</h1>
+            <div className='w-full top-[20%] p-4 ml-5 md:p-8 absolute'>
+                    <h1 className="text-3xl ml-5 mb-2 md:text-5xl">{movie?.title}</h1>
                 <div>
                     <button className="border bg-gray-300 text-balck border-gray-300 py-2 px-5 ml-4">
                         Play
@@ -33,6 +33,9 @@ export const Main = () => {
                         Watch Later
                     </button>
                 </div>
+                <p className="ml-5 py-5 text-gray-300 text-sm">
+                    Release Date: {movie.release_date}
+                </p>
             </div>
         </div>
     </div>
