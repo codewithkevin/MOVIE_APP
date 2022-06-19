f8d0ed2cd32a439f9a6e50753afaec39fe8d97d9
codewithkevin <oseikelvin709@gmail.com>
2022-06-19 07:34:32 +0000

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,18 @@
+import axios from 'axios'
 import React from 'react'
+import request from '../Request'
 
 export const Main = () => {
 
-    const [movies, setMovies] = React.useState([])
+  const [movies, setMovies] = React.useState([])
+
+    React.useEffect(() => {
+        axios.get(request.requestPopular).then((response) => {
+            setMovies(response.data.results)
+        })
+    }, [])
+
+    console.log(movies)
 
   return (
     <div>Main</div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ export const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4
     z-{100} w-full absolute'>
-        <h1 className='text-red-600 text-4xl 
-        font-bold cursor-pointer'>NETFLIX</h1>
+        <h1 className='text-blue-200 text-4xl 
+        font-bold cursor-pointer'>TWIFLEX</h1>
 
         <div>
           <button className='text-white pr-4'>Sign In</button>
