5ba1e687cf8f55bdff1dcb4a363878d00ce38a75
codewithkevin <oseikelvin709@gmail.com>
2022-06-19 11:57:56 +0000

    adding row content

---
+++ src/components/Row.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Row = () => {
+  return (
+    <div>Row</div>
+  )
+}
+
+export default Row;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Main from '../components/Main' 
+import Row from '../components/Row';
+import request from '../Request';
 
 export const Home = () => {
   return (
     <>
         <Main />
+        <Row title='Up Coming' fetchURL={request.requestUpcoming}/>
+        <Row title='Up Coming' fetchURL={request.requestUpcoming}/>
+        <Row title='Up Coming' fetchURL={request.requestUpcoming}/>
+        <Row title='Up Coming' fetchURL={request.requestUpcoming}/>
+        <Row title='Up Coming' fetchURL={request.requestUpcoming}/>
     </>
   )
 }
