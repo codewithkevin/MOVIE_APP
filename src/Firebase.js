69976a3ea6f88b259f5926321e424e829b5ac91b
codewithkevin <oseikelvin709@gmail.com>
2022-06-20 20:23:21 +0000

    setting up firestore

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"; // eslint-disable-next-line
 import { getAuth } from "firebase/auth"; 
+import { getFirestore } from "firebase/firestore";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,3 +20,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig); 
 export const auth = getAuth(app); 
+export const firestore = getFirestore(app);
